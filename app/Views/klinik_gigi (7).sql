40a2ec8cc0cf1311100b32e970eb2c11e1e40bc6
budisusanto1 <132729789+budisusanto1@users.noreply.github.com>
2025-05-16 13:56:36 +0700

    Add files via upload

---
+++ app/Views/klinik_gigi (7).sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2025 at 08:56 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `klinik_gigi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+-- Error reading structure for table klinik_gigi.booking: #1932 - Table 'klinik_gigi.booking' doesn't exist in engine
+-- Error reading data for table klinik_gigi.booking: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`booking`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+-- Error reading structure for table klinik_gigi.dokter: #1932 - Table 'klinik_gigi.dokter' doesn't exist in engine
+-- Error reading data for table klinik_gigi.dokter: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`dokter`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal`
+--
+-- Error reading structure for table klinik_gigi.jadwal: #1932 - Table 'klinik_gigi.jadwal' doesn't exist in engine
+-- Error reading data for table klinik_gigi.jadwal: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`jadwal`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `layanan`
+--
+-- Error reading structure for table klinik_gigi.layanan: #1932 - Table 'klinik_gigi.layanan' doesn't exist in engine
+-- Error reading data for table klinik_gigi.layanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`layanan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log_activity`
+--
+-- Error reading structure for table klinik_gigi.log_activity: #1932 - Table 'klinik_gigi.log_activity' doesn't exist in engine
+-- Error reading data for table klinik_gigi.log_activity: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`log_activity`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+-- Error reading structure for table klinik_gigi.pasien: #1932 - Table 'klinik_gigi.pasien' doesn't exist in engine
+-- Error reading data for table klinik_gigi.pasien: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`pasien`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+-- Error reading structure for table klinik_gigi.pembayaran: #1932 - Table 'klinik_gigi.pembayaran' doesn't exist in engine
+-- Error reading data for table klinik_gigi.pembayaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`pembayaran`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaturan_app`
+--
+-- Error reading structure for table klinik_gigi.pengaturan_app: #1932 - Table 'klinik_gigi.pengaturan_app' doesn't exist in engine
+-- Error reading data for table klinik_gigi.pengaturan_app: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`pengaturan_app`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table klinik_gigi.user: #1932 - Table 'klinik_gigi.user' doesn't exist in engine
+-- Error reading data for table klinik_gigi.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `klinik_gigi`.`user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
